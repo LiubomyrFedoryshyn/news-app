e9d42ca3c051b63d76ff818ff1f82e5dda7f700b
Liubomyr Fedoryshyn <liubomyr.fedoryshyn@axiosint.com>
2023-04-26 15:33:51 +0300

    [fix] Update interval time up to 5 min

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,4 +2,4 @@ export const POINTS_URL: string = "https://news.ycombinator.com/item?id=";
 export const AUTHOR_URL: string = "https://news.ycombinator.com/user?id=";
 export const DOTS: string = "...";
 export const NEWS_API_LIMIT: number = 1000;
-export const NEWS_API_INTERVAL: number = 50000;
+export const NEWS_API_INTERVAL: number = 300000; // 5 min
